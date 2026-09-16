3d4a75358ba34e1c04619cf7ea2682296381bbae
K0311 <jckiminzw1212@gmail.com>
2026-09-16 20:33:41 +0700

    A7: multiply sum, red

--- test_kitchen.py
+++ test_kitchen.py
@@ -39,3 +39,11 @@ def test_addition_across_units():
     converter = Converter()
 
     assert converter.reduce(total, "g") == grams(228.35)
+
+def test_sum_can_be_multiplied():
+    total = grams(200).plus(ounces(1))
+    doubled = total.times(2)
+
+    converter = Converter()
+
+    assert converter.reduce(doubled, "g") == grams(456.70)
